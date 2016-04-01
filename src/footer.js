2b90ff32f2d19a12855d72660ff33e933f8ff6ba
Eminanza <michaeltihon@gmail.com>
2016-04-01 08:15:42 +0200

    v2.0.5

--- src/footer.js
+++ src/footer.js
@@ -7,7 +7,7 @@ var Footer = React.createClass({
         <div className="container-fluid">
           <ul className="nav navbar-nav">
             <li>
-              <a href="#">(c) 2016 Timigula</a>
+              <a href="#">(c) 2016 Timigula - Test</a>
             </li>
           </ul>
 
